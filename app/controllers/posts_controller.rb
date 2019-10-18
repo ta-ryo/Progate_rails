8e6b0a13e5d7ee981b9074e377d3f2bc44d919b6
Ta-ryo <e165716@ie.u-ryukyu.ac.jp>
2019-10-18 18:34:02 +0900

    fourth_class comp

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+   
     @posts = Post.all.order(created_at: :desc)
   end
 
@@ -14,6 +15,23 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(content: params[:content])
     @post.save
+    redirect_to("/posts/index")
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
     redirect_to("/posts/index")
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'posts/:id' => "posts#show"
 
   post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
 
   get "/" => "home#top"
   get "about" => "home#about"
