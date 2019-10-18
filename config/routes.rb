d79140f839d65313df528aebe2a563ca1445327e
Ta-ryo <e165716@ie.u-ryukyu.ac.jp>
2019-10-18 19:21:48 +0900

    fifth_class comp

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  get "users/:id" => "users#show"
+  post "/users/create" => "users#create"
+  get "/signup" => "users#new"
+
+
   get 'posts/index' => "posts#index"
   get 'posts/new' => "posts#new"
   get 'posts/:id' => "posts#show"
-
   post "posts/create" => "posts#create"
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_082444) do
+ActiveRecord::Schema.define(version: 2019_10_18_095220) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
